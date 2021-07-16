75e69c1bd74ec6099104cead14f8bbe3cd0ec820
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2021-07-16 20:44:03 +0600

    removing .git from client folder

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getAllUsers } = require('../controller/userController');
 
+
 const router  = express.Router();
 
 
 
-router.route('/').get(getAllUsers)
+router.route('/').get(getAllUsers
+    )
